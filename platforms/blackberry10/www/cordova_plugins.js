3b17a46aacde78b78e77609ec2e4f5c0828b41c7
Jorge del Casar <jorge.casar@gmail.com>
2013-09-07 20:36:30 +0200

    Reinit repo

--- platforms/blackberry10/www/cordova_plugins.js
+++ platforms/blackberry10/www/cordova_plugins.js
@@ -1,18 +1,3 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-    {
-        "file": "plugins\\org.apache.cordova.core.vibration\\www\\vibration.js",
-        "id": "org.apache.cordova.core.vibration.notification",
-        "merges": [
-            "navigator.notification"
-        ]
-    },
-    {
-        "file": "plugins\\org.apache.cordova.core.dialogs\\www\\notification.js",
-        "id": "org.apache.cordova.core.dialogs.notification",
-        "merges": [
-            "navigator.notification"
-        ]
-    }
-]
+module.exports = []
 });
