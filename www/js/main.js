96b37174c24b8d31c621f2c83f8dc7abbdb53028
Jorge del Casar <jorge.casar@gmail.com>
2013-09-08 09:51:53 +0200

    main.js first commit
    - Defer device ready event
    - Start app when finish

---
+++ www/js/main.js
@@ -0,0 +1,28 @@
+require(
+	[ 'jquery', 'backbone', 'app', 'router'],
+	function($, Backbone, App, router) {
+		'use strict';
+		
+		var deviceReadyDeferred = $.Deferred();
+
+		function start() {
+			console.log('Main.start:');
+		}
+
+		function onDeviceReady(isDevice) {
+			console.log('Main.onDeviceReady: running on a device: ' + (isDevice !== false));
+			deviceReadyDeferred.resolve();
+		}
+
+		if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|BB19|IEMobile)/)) {
+			// This is running on a device so waiting for deviceready event
+			document.addEventListener('deviceready', onDeviceReady, false);
+		} else {
+			// On desktop don't have to wait for anything
+			onDeviceReady(false);
+		}
+
+		// Only proceed when PhoneGap and jqm are loaded
+		$.when(deviceReadyDeferred).then(start);
+	}
+);
