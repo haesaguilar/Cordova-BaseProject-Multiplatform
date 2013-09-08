85fca1e246b8ebbb17463c8a9da9ff83a1ceccc4
Jorge del Casar <jorge.casar@gmail.com>
2013-09-08 14:20:03 +0200

    Authors as CompositeView with its own template

--- www/js/views/authorsView.js
+++ www/js/views/authorsView.js
@@ -1,10 +1,11 @@
 define(
-	[ 'jquery', 'underscore', 'handlebars', 'backbone', 'app', 'views/authorView' ],
-	function($, _, Handlebars, Backbone, App, AuthorView) {
+	[ 'jquery', 'underscore', 'handlebars', 'backbone', 'app', 'views/authorView', 'text!templates/authors.hbs'  ],
+	function($, _, Handlebars, Backbone, App, AuthorView, authorsTemplate) {
 		'use strict';
-		var AuthorsView = Backbone.Marionette.CollectionView.extend({
+		var AuthorsView = Backbone.Marionette.CompositeView.extend({
+			template: Handlebars.compile(authorsTemplate),
 			itemView: AuthorView,
-			tagName: 'ul',
+			itemViewContainer: '#authorsList',
 			initialize: function(options) {
 				this.collection.on('change', this.render, this);
 			},
